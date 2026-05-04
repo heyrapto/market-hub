8476e0aafd296776d111c97dde6b542c005a3b55
heyrapto <raptomiofw@gmail.com>
2026-05-04 20:39:00 +0100

    part II of previous commit

--- apps/server/src/routes/product.route.ts
+++ apps/server/src/routes/product.route.ts
@@ -1,5 +1,26 @@
+import { Router } from "express";
+import {
+  createProduct,
+  updateProduct,
+  getAllProducts,
+  getProductById,
+  deleteMultipleProducts,
+  deleteProduct,
+} from "../controllers/product.controller";
+
+const router = Router();
+
 // create a product
+router.post("/create", createProduct);
 // get all products
+router.post("/", getAllProducts);
+// get a product by id
+router.post("/:id", getProductById);
 // update a product
+router.post("/update/:id", updateProduct);
 // delete a product
+router.post("/delete/:id", deleteProduct);
 // delete multiple products
+router.post("/delete/multiple", deleteMultipleProducts);
+
+export default router;
